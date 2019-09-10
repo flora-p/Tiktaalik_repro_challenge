131ec2c0f8e32e9f8f956be1430099f37c37d25e
Emma Wilkinson <ewilkinson@uchicago.edu>
2019-09-10 16:48:01 -0400

    Mean and dLGP function continued

--- Reproducibility.R
+++ Reproducibility.R
@@ -8,6 +8,11 @@ $P_n(k)_{n\to \infty} = \frac{\lambda^k}{k!} e^{-\lambda}$
 ##Install Package
 install.packages("RMKdiscrete")
 ##Load Library
+library(RMKdiscrete)
+
+##Calculate mean (lambda1)
+sum(weevil_data$C_count_of_beans_with_k_eggs)
+sum(weevil_data$k_number_of_eggs)
+
 ##Use function dLGP from RMKdiscrete
-
-
+output <- dLGP(x = weevil_data$k_number_of_eggs, theta = mean, lambda = 0)

9b30dbf21d5381cc8edd1272e440876c96eca894
Emma Wilkinson <ewilkinson@uchicago.edu>
2019-09-10 16:33:26 -0400

    dLGP function from RMKdiscrete package

--- Reproducibility.R
+++ Reproducibility.R
@@ -1,5 +1,13 @@
 ##Title : Data Reproducibility Problem Set
 getwd()
 weevil_data <- read.csv(file =
-           "mitchell_weevil_egg_data_1975.csv")
+           "mitchell_weevil_egg_data_1975.csv", )
+##Poisson distribution
+$P_n(k)_{n\to \infty} = \frac{\lambda^k}{k!} e^{-\lambda}$
+
+##Install Package
+install.packages("RMKdiscrete")
+##Load Library
+##Use function dLGP from RMKdiscrete
+
 

e44ac92a08747d8b46314f9a21928cf2b320482f
Flora <flora.plessier@gmail.com>
2019-09-10 16:49:20 -0400

    basic plot added - for merge conflict res

--- weevil.R
+++ weevil.R
@@ -9,4 +9,16 @@ getwd()
 # Opening Weevil dataframe
 weevil_data <- read.csv(file = "mitchell_weevil_egg_data_1975.csv")
 
+## library RMKdiscrete
+library(RMKdiscrete)
 
+## library for plotting 
+library(ggplot2)
+
+## basic plot with real datapoints 
+plot_points <- ggplot(weevil_data, aes(x=k_number_of_eggs, y=C_count_of_beans_with_k_eggs))+
+  geom_point(size=3) +
+  theme_minimal()
+plot_points
+
+  

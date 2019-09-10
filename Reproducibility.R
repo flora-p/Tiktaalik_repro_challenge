312e89e7baaa0522f45b8a27e4e2835baf5974b6
Christopher <ckluong95@gmail.com>
2019-09-10 16:11:06 -0500

    Mean of eggs

--- Reproducibility.R
+++ Reproducibility.R
@@ -1,7 +1,7 @@
 ##Title : Data Reproducibility Problem Set
 getwd()
-weevil_data <- read.csv(file =
-           "mitchell_weevil_egg_data_1975.csv", )
+weevil_data <- read.csv(file = "mitchell_weevil_egg_data_1975.csv")
+
 ##Poisson distribution
 $P_n(k)_{n\to \infty} = \frac{\lambda^k}{k!} e^{-\lambda}$
 
@@ -11,8 +11,18 @@ install.packages("RMKdiscrete")
 library(RMKdiscrete)
 
 ##Calculate mean (lambda1)
-sum(weevil_data$C_count_of_beans_with_k_eggs)
-sum(weevil_data$k_number_of_eggs)
+total_beans <- sum(weevil_data$C_count_of_beans_with_k_eggs)
 
 ##Use function dLGP from RMKdiscrete
 output <- dLGP(x = weevil_data$k_number_of_eggs, theta = mean, lambda = 0)
+
+# Creating vectors of data columns
+weevil_egg_number <- c(weevil_data$k_number_of_eggs)
+eggs_found_in_beans <- c(weevil_data$C_count_of_beans_with_k_eggs)
+
+# Sum to find the total number of eggs
+total_eggs <- sum(weevil_egg_number * eggs_found_in_beans)
+
+# Mean of eggs per bean
+mean_of_eggs <- total_eggs/total_beans
+

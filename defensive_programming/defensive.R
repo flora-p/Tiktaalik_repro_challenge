809932da182f99dbb61c43bfcde11464959af6ef
XinyiLi <xinyil1@uchicago.edu>
2019-09-13 20:02:12 -0400

    defensive version 1.2

--- defensive_programming/defensive.R
+++ defensive_programming/defensive.R
@@ -1,6 +1,6 @@
 data = read.csv("/Users/xli/Documents/xinyi/UChicago/BSD-QBio5/tutorials/defensive_programming/data/subtype_counts.csv",stringsAsFactors = F)
 
-calculate_prob <-function (birthyear, prob_data, subtype)
+calculate_prob <-function (dz_prev, birthyear, prob_data, subtype)
 {
  #check if birthyear is positive and integer 
   if (birthyear < 0 | birthyear!=round(birthyear)) 
@@ -11,9 +11,15 @@ calculate_prob <-function (birthyear, prob_data, subtype)
   #check if your subtype is in the dataset 
   if (!subtype %in% colnames(prob_data))
     stop("We don't have your subtype information")
+  #check if row sums are zero
+  if (sum(rowSums(prob_data[,c(2:4)])==0)!=0)
+    stop("The row sum of count data should not be zero")
+  #check if disease prevalence is 0-1 
+  if (dz_prev >1 | dz_prev < 0 )
+    stoop("disease prevalence should be between 0-1")
  
   #find the maximum year in calculating probability  
-  endyear =max(prob_data$Year)
+  endyear = max(prob_data$Year)
   #find the number of years from birth to the maximum year
   year_range = endyear-birthyear 
   #initialize an empty probability vector 
@@ -24,11 +30,13 @@ calculate_prob <-function (birthyear, prob_data, subtype)
   {
     #first year probability: 0.28*P 
     if (i==1)
-      prob[i] = 0.28* data[data$Year==birthyear,subtype_col]/sum(data[data$Year==birthyear,c(2:4)])
+      prob[i] = dz_prev * data[data$Year==birthyear,subtype_col]/sum(data[data$Year==birthyear,c(2:4)])
     #the rest of year probability: last_year_probability + (1-0.28)^(n-1)*0.28*P 
     else 
-      prob[i] = prob[i-1]+ (1-0.28)^(i-1)* 0.28 *
+      prob[i] = prob[i-1]+ (1-dz_prev)^(i-1)* dz_prev *
         data[data$Year==(birthyear+i-1),subtype_col]/sum(data[data$Year==(birthyear+i-1),c(2:4)])
   }
   return(prob)
 }
+
+calculate_prob(0.28, 1967, data, "H1N1")
